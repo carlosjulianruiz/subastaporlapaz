9610300d2e85d689dc6e9c8562e7fd892383a264
CARLOS JULIAN RUIZ <carlos.julian@banexport.com>
2025-05-27 22:53:21 -0500

    ajuste en cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     supportFile: "cypress/support/e2e.ts",
-    setupNodeEvents(on, config) {
+    setupNodeEvents(_on, _config) {
       // Puedes agregar tareas si es necesario
     },
   },
