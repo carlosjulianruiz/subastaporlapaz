68e5eec3ccb5283b105cfe75097aa9f2ca389b0f
CARLOS JULIAN RUIZ <carlos.julian@banexport.com>
2025-05-27 22:35:55 -0500

    varios ajustes

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,29 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      const isDev = config.watchForFileChanges;
-      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
-
-      const configOverrides = {
-        baseUrl: `http://localhost:${port}`,
-        screenshotOnRunFailure: !process.env.CI,
-      };
-
-      on("task", {
-        log: (message: unknown): null => {
-          console.log(message);
-          return null;
-        },
-      });
-
-      return { ...config, ...configOverrides };
-    },
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
+      // implement node event listeners here
     },
   },
 });
